463b3a7cc6ad497656dce35fbd6eb4d89eb7febc
Danial Hosseintabar <danial.ht10@gmail.com>
2025-01-20 14:08:31 -0500

    refactor

--- main.py
+++ main.py
@@ -3,14 +3,8 @@ from utils import get_nonempty_sections, print_section
 from rich.console import Console
 
 def main():
-    """
-    Getting the news from newsletter website(s) and printing them
-    on the terminal screen.
-    """
     today_date = date.today()
-    
     url = "https://tldr.tech/tech/" + today_date.strftime("%Y-%m-%d")
-    print(url)
     console = Console()
 
     for section in get_nonempty_sections(url):
